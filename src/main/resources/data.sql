b3b10ab97bb9ba871bd57e2809b275b5bacf035b
PavlovAlexL <dumadumae@mail.ru>
2019-04-12 13:05:47 +0300

    Обновил схему базы данных, добавил справочник, внешние ключи и др.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,44 @@
-INSERT INTO Docks (code, name) VALUES (03, 'Свидетельство о рождении');
-INSERT INTO Docks (code, name) VALUES (07, 'Военный билет');
-INSERT INTO Docks (code, name) VALUES (08, 'Временное удостоверение, выданное взамен военного билета');
-INSERT INTO Docks (code, name) VALUES (10, 'Паспорт иностранного гражданина');
-INSERT INTO Docks (code, name) VALUES (11, 'Свидетельство о рассмотрении ходатайства о признании лица беженцем на территории РФ по существу');
-INSERT INTO Docks (code, name) VALUES (12, 'Вид на жительство в Российской Федерации');
-INSERT INTO Docks (code, name) VALUES (13, 'Удостоверение беженца');
-INSERT INTO Docks (code, name) VALUES (15, 'Разрешение на временное проживание в Российской Федерации');
-INSERT INTO Docks (code, name) VALUES (18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
-INSERT INTO Docks (code, name) VALUES (21, 'Паспорт гражданина Российской Федерации');
-INSERT INTO Docks (code, name) VALUES (23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
-INSERT INTO Docks (code, name) VALUES (24, 'Удостоверение личности военнослужащего Российской Федерации');
-INSERT INTO Docks (code, name) VALUES (91, 'Иные документы');
+INSERT INTO Dockument (code, name) VALUES (03, 'Свидетельство о рождении');
+INSERT INTO Dockument (code, name) VALUES (07, 'Военный билет');
+INSERT INTO Dockument (code, name) VALUES (08, 'Временное удостоверение, выданное взамен военного билета');
+INSERT INTO Dockument (code, name) VALUES (10, 'Паспорт иностранного гражданина');
+INSERT INTO Dockument (code, name) VALUES (11, 'Свидетельство о рассмотрении ходатайства о признании лица беженцем на территории РФ по существу');
+INSERT INTO Dockument (code, name) VALUES (12, 'Вид на жительство в Российской Федерации');
+INSERT INTO Dockument (code, name) VALUES (13, 'Удостоверение беженца');
+INSERT INTO Dockument (code, name) VALUES (15, 'Разрешение на временное проживание в Российской Федерации');
+INSERT INTO Dockument (code, name) VALUES (18, 'Свидетельство о предоставлении временного убежища на территории Российской Федерации');
+INSERT INTO Dockument (code, name) VALUES (21, 'Паспорт гражданина Российской Федерации');
+INSERT INTO Dockument (code, name) VALUES (23, 'Свидетельство о рождении, выданное уполномоченным органом иностранного государства');
+INSERT INTO Dockument (code, name) VALUES (24, 'Удостоверение личности военнослужащего Российской Федерации');
+INSERT INTO Dockument (code, name) VALUES (91, 'Иные документы');
 
-INSERT INTO Countries (code, name) VALUES (616,	'ПОЛЬША');
-INSERT INTO Countries (code, name) VALUES (643,	'РОССИЯ');
-INSERT INTO Countries (code, name) VALUES (804,	'УКРАИНА');
-INSERT INTO Countries (code, name) VALUES (792,	'ТУРЦИЯ');
-INSERT INTO Countries (code, name) VALUES (578,	'НОРВЕГИЯ');
-INSERT INTO Countries (code, name) VALUES (528,	'НИДЕРЛАНДЫ');
-INSERT INTO Countries (code, name) VALUES (484,	'МЕКСИКА');
-INSERT INTO Countries (code, name) VALUES (492,	'МОНАКО');
-INSERT INTO Countries (code, name) VALUES (496,	'МОНГОЛИЯ');
-INSERT INTO Countries (code, name) VALUES (434,	'ЛИВИЯ');
-INSERT INTO Countries (code, name) VALUES (438,	'ЛИХТЕНШТЕЙН');
-INSERT INTO Countries (code, name) VALUES (440,	'ЛИТВА');
-INSERT INTO Countries (code, name) VALUES (442,	'ЛЮКСЕМБУРГ');
-INSERT INTO Countries (code, name) VALUES (276,	'ГЕРМАНИЯ');
+INSERT INTO Country (code, name) VALUES (616,	'ПОЛЬША');
+INSERT INTO Country (code, name) VALUES (643,	'РОССИЯ');
+INSERT INTO Country (code, name) VALUES (804,	'УКРАИНА');
+INSERT INTO Country (code, name) VALUES (792,	'ТУРЦИЯ');
+INSERT INTO Country (code, name) VALUES (578,	'НОРВЕГИЯ');
+INSERT INTO Country (code, name) VALUES (528,	'НИДЕРЛАНДЫ');
+INSERT INTO Country (code, name) VALUES (484,	'МЕКСИКА');
+INSERT INTO Country (code, name) VALUES (492,	'МОНАКО');
+INSERT INTO Country (code, name) VALUES (496,	'МОНГОЛИЯ');
+INSERT INTO Country (code, name) VALUES (434,	'ЛИВИЯ');
+INSERT INTO Country (code, name) VALUES (438,	'ЛИХТЕНШТЕЙН');
+INSERT INTO Country (code, name) VALUES (440,	'ЛИТВА');
+INSERT INTO Country (code, name) VALUES (442,	'ЛЮКСЕМБУРГ');
+INSERT INTO Country (code, name) VALUES (276,	'ГЕРМАНИЯ');
+
+INSERT INTO Position (code, name) VALUES (1, 'Директор');
+INSERT INTO Position (code, name) VALUES (2, 'Бухгалтер');
+INSERT INTO Position (code, name) VALUES (3, 'Начальник отдела');
+INSERT INTO Position (code, name) VALUES (4, 'Менеджер');
+INSERT INTO Position (code, name) VALUES (5, 'Администратор');
+INSERT INTO Position (code, name) VALUES (6, 'Специалист');
+INSERT INTO Position (code, name) VALUES (7, 'Работник');
+
+
+INSERT INTO Organization (name, fullName, inn, kpp, address, phone, isActive) VALUES ('Белл', 'ООО "Белл Интегратор"','1111111111','222222222','г. Москва, ул. Южнопортовая','84957778899','true');
+INSERT INTO Organization (name, fullName, inn, kpp, address, phone, isActive) VALUES ('Крок', 'ООО "Крок"','3333333333','222232222','г. Москва, ул. Таганская','84953778899','true');
+
+INSERT INTO Office (orgId) VALUES ('2');
+
+INSERT INTO User (officeId, first_Name, position) VALUES (1, 'Sasha', 'Менеджер');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,59 +1,69 @@
+CREATE TABLE IF NOT EXISTS Country (
+  code                INTEGER NOT NULL PRIMARY KEY ,
+  name                VARCHAR(50) NOT NULL
+);
+COMMENT ON TABLE Country IS 'Спарвочник стран';
+
+CREATE TABLE IF NOT EXISTS Dockument (
+  code                INTEGER NOT NULL PRIMARY KEY ,
+  name                VARCHAR(100) NOT NULL
+);
+COMMENT ON TABLE Dockument IS 'Спарвочник документов';
+
+CREATE TABLE IF NOT EXISTS Position (
+  code                INTEGER NOT NULL PRIMARY KEY ,
+  name                VARCHAR(100) NOT NULL
+);
+COMMENT ON TABLE Position IS 'Спарвочник должностей';
+
+
+
+
+
+
+
 CREATE TABLE IF NOT EXISTS Organization (
-  id          INTEGER PRIMARY KEY,
-  version     INTEGER NOT NULL,
+  id          INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  version     INTEGER,
 
   name        VARCHAR(50) NOT NULL,
   fullName    VARCHAR(50) NOT NULL,
   inn         VARCHAR(10) NOT NULL,
   kpp         VARCHAR(9)  NOT NULL,
-  address     VARCHAR(50) NOT NULL,
-  phone       VARCHAR(11) NOT NULL,
-  isActive    INTEGER NOT NULL
+  address     VARCHAR(100) NOT NULL,
+  phone       VARCHAR(11) ,
+  isActive    VARCHAR(5) DEFAULT 'true'
 );
-
 COMMENT ON TABLE Organization IS 'Организация';
 
 
 CREATE TABLE IF NOT EXISTS Office (
-  id          INTEGER PRIMARY KEY,
-  version     INTEGER NOT NULL,
+  id          INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  version     INTEGER,
 
-  name        VARCHAR(50) NOT NULL,
-  address     VARCHAR(50) NOT NULL,
-  phone       INTEGER NOT NULL,
-  isActive    INTEGER NOT NULL
+  orgId       INTEGER NOT NULL REFERENCES Organization(id),
+  name        VARCHAR(50),
+  address     VARCHAR(100),
+  phone       VARCHAR(11),
+  isActive    VARCHAR(5) DEFAULT 'true'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS User (
-  id                  INTEGER PRIMARY KEY,
-  version             INTEGER NOT NULL,
+  id                  INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  version             INTEGER,
 
+  officeId            INTEGER NOT NULL REFERENCES Office(id),
   first_Name           VARCHAR(50) NOT NULL,
-  second_Name          VARCHAR(50) NOT NULL,
-  middle_Name          VARCHAR(50) NOT NULL,
-  position             VARCHAR(50) NOT NULL,
-  phone                INTEGER     NOT NULL,
-  doc_Name             VARCHAR(50) NOT NULL,
-  doc_Number           INTEGER     NOT NULL,
-  doc_Date             DATE        NOT NULL,
-  citizenship_Name     VARCHAR(50) NOT NULL,
-  citizenship_Code     INTEGER     NOT NULL,
-  is_Identified        INTEGER     NOT NULL
+  second_Name          VARCHAR(50) ,
+  middle_Name          VARCHAR(50) ,
+  position             VARCHAR(50) NOT NULL REFERENCES Position(name),
+  phone                VARCHAR(11) ,
+  doc_Name             VARCHAR(50) ,
+  doc_Number          INTEGER REFERENCES Dockument(code),
+  doc_Date             DATE        ,
+  citizenship_Name     VARCHAR(50) ,
+  citizenship_Code    INTEGER      ,
+  is_Identified        VARCHAR(5) DEFAULT 'true'
 );
 COMMENT ON TABLE User IS 'Сотрудник';
-
-CREATE TABLE IF NOT EXISTS Countries (
-  code                INTEGER NOT NULL,
-  name                VARCHAR(50) NOT NULL
-);
-COMMENT ON TABLE Countries IS 'Спарвочник стран';
-
-CREATE TABLE IF NOT EXISTS Docks (
-  code                INTEGER NOT NULL,
-  name                VARCHAR(100) NOT NULL
-);
-COMMENT ON TABLE Docks IS 'Спарвочник документов';
-
-
-
